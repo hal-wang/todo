879c24081b94456297697b364c2f28ef94a030e4
hal-wang <hi@hal.wang>
2021-04-17 21:26:29 +0800

    chore: deploy

--- todo-api/doc.sh
+++ todo-api/doc.sh
@@ -9,5 +9,9 @@ tsc
 
 find dist -name "*.d.ts" |xargs rm -rf
 
+if [ ! -d "../todo-docs/docs/api" ]; then
+  mkdir ../todo-docs/docs/api
+fi
+
 cba-doc dist/controllers ../todo-docs/docs/api/README.md docConfigs/base.json
 rm -rf dist
